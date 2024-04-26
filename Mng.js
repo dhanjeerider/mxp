e8f667dd3566e4ff649c878e2323a22c96b5dec4
Dhanjee Rider <102199585+dhanjeerider@users.noreply.github.com>
2024-04-26 15:09:32 +0530

    Create Mng.js

---
+++ Mng.js
@@ -0,0 +1,6 @@
+if (window.location.href.startsWith("https://dkhek.blogspot.com/")) {
+  document.getElementById("main").style.display = "block";
+
+} else {
+  document.getElementById("main").style.display = "none";
+  alert("Credit expired.");}
