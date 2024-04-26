8f4ad9a11081b8fc9f7dedf2766271a257b00a2a
Dhanjee Rider <102199585+dhanjeerider@users.noreply.github.com>
2024-04-26 15:27:17 +0530

    Update Mng.js

--- Mng.js
+++ Mng.js
@@ -1,6 +1,6 @@
 if (window.location.href.startsWith("https://dkhek.blogspot.com/")) {
-  document.getElementById("main").style.display = "block";
-
-} else {
   document.getElementById("main").style.display = "none";
-  alert("Credit expired.");}
+  alert("Credit expired.");
+} else {
+  window.location.href = "https://www.google.com";
+}
